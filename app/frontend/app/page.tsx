151e286ab8761447f356bbfd58ad7d8ff02e3191
nkien0204 <nkien0204@gmail.com>
2024-12-05 23:10:55 +0700

    transfer token

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -4,8 +4,10 @@ import {
     AnchorProvider,
     Program,
 } from "@coral-xyz/anchor";
-import idl from "../../../target/idl/mycounter.json"; // Replace with the path to your IDL file
-import { Mycounter } from "../../../target/types/mycounter";
+//import idl from "../../../target/idl/mycounter.json"; // Replace with the path to your IDL file
+import idl from "../../../target/idl/transfer_tokens.json"; // Replace with the path to your IDL file
+//import { Mycounter } from "../../../target/types/mycounter";
+import { TransferTokens } from "../../../target/types/transfer_tokens";
 import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 import { web3 } from "@coral-xyz/anchor";
@@ -21,6 +23,8 @@ import { PhantomWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import * as anchor from '@coral-xyz/anchor';
+//import { getAssociatedTokenAddressSync } from '@solana/spl-token';
 
 const App: React.FC = () => {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
@@ -75,22 +79,24 @@ function Home() {
     }
     console.log("wallet", wallet);
     const provider = new AnchorProvider(connection, wallet, {});
-    const program = new Program(idl as Mycounter, provider);
+    const program = new Program(idl as TransferTokens, provider);
     if (!program) {
         console.log("Program not found");
         return;
     }
+    console.log("program", program.programId.toString());
 
-    // Example function to call a method from your Solana program
     const callMethod = async () => {
         try {
             const user = program.provider.publicKey || "";
             console.log("user", user);
             const tx = await program.methods
-                .initialize()
+                .createToken("solanaVapeXX", "VAPX", "https://images.unsplash.com/photo-1642576422624-fd0df2b084d3")
                 .accounts({
-                    counter: counterAccount.publicKey,
-                    user: program.provider.publicKey || "",
+                    payer: wallet.publicKey,
+                    mintAccount: counterAccount.publicKey
+                    //counter: counterAccount.publicKey,
+                    //user: program.provider.publicKey || "",
                     //systemProgram: anchor.web3.SystemProgram.programId,
                 })
                 .signers([counterAccount])
@@ -101,15 +107,24 @@ function Home() {
         }
     };
 
-    const increment = async () => {
+    const mint = async () => {
         try {
             const user = program.provider.publicKey || "";
             console.log("user", user);
+            const amount = new anchor.BN(10000);
+            //const recepientTokenAddress = getAssociatedTokenAddressSync(counterAccount.publicKey, wallet.publicKey);
             const tx = await program.methods
-                .increment()
+                .mintToken(amount)
                 .accounts({
-                    counter: counterAccount.publicKey,
+                    mintAuthority: wallet.publicKey,
+                    recipient: wallet.publicKey,
+                    mintAccount: counterAccount.publicKey,
+                    //associatedTokenAccount: recepientTokenAddress
+                    //counter: counterAccount.publicKey,
+                    //user: program.provider.publicKey || "",
+                    //systemProgram: anchor.web3.SystemProgram.programId,
                 })
+                //.signers([counterAccount])
                 .rpc();
             console.log('Transaction successful:', tx);
         } catch (error) {
@@ -117,31 +132,77 @@ function Home() {
         }
     };
 
-    const decrement = async () => {
-        try {
-            const user = program.provider.publicKey || "";
-            console.log("user", user);
-            const tx = await program.methods
-                .decrement()
-                .accounts({
-                    counter: counterAccount.publicKey,
-                })
-                .rpc();
-            console.log('Transaction successful:', tx);
-        } catch (error) {
-            console.error('Transaction failed:', error);
-        }
-    };
+    // Example function to call a method from your Solana program
+    //const callMethod = async () => {
+    //    try {
+    //        const user = program.provider.publicKey || "";
+    //        console.log("user", user);
+    //        const tx = await program.methods
+    //            .initialize()
+    //            .accounts({
+    //                counter: counterAccount.publicKey,
+    //                user: program.provider.publicKey || "",
+    //                //systemProgram: anchor.web3.SystemProgram.programId,
+    //            })
+    //            .signers([counterAccount])
+    //            .rpc();
+    //        console.log('Transaction successful:', tx);
+    //    } catch (error) {
+    //        console.error('Transaction failed:', error);
+    //    }
+    //};
 
-    const getAccount = async () => {
-        try {
-            const account = await program.account.counter.fetch(counterAccount.publicKey);
-            console.log('Account:', account);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
+    //const increment = async () => {
+    //    try {
+    //        const user = program.provider.publicKey || "";
+    //        console.log("user", user);
+    //        const tx = await program.methods
+    //            .increment()
+    //            .accounts({
+    //                counter: counterAccount.publicKey,
+    //            })
+    //            .rpc();
+    //        console.log('Transaction successful:', tx);
+    //    } catch (error) {
+    //        console.error('Transaction failed:', error);
+    //    }
+    //};
+    //
+    //const decrement = async () => {
+    //    try {
+    //        const user = program.provider.publicKey || "";
+    //        console.log("user", user);
+    //        const tx = await program.methods
+    //            .decrement()
+    //            .accounts({
+    //                counter: counterAccount.publicKey,
+    //            })
+    //            .rpc();
+    //        console.log('Transaction successful:', tx);
+    //    } catch (error) {
+    //        console.error('Transaction failed:', error);
+    //    }
+    //};
 
+    //const getAccount = async () => {
+    //    try {
+    //        const account = await program.account.counter.fetch(counterAccount.publicKey);
+    //        console.log('Account:', account);
+    //    } catch (error) {
+    //        console.error('Error:', error);
+    //    }
+    //}
+
+    //<div>
+    //    <button onClick={increment}>Increment</button>
+    //</div>
+    //<div>
+    //    <button onClick={decrement}>Decrement</button>
+    //</div>
+    //<div>
+    //    <button onClick={getAccount}>Get account</button>
+    //</div>
+    //F1WqxTNeJo9BXmkSCeryxghRERxztXiWHVF4QTQTFpWr
     return (
         <div>
             <div>
@@ -149,13 +210,7 @@ function Home() {
                 <button onClick={callMethod}>Call Method</button>
             </div>
             <div>
-                <button onClick={increment}>Increment</button>
-            </div>
-            <div>
-                <button onClick={decrement}>Decrement</button>
-            </div>
-            <div>
-                <button onClick={getAccount}>Get account</button>
+                <button onClick={mint}>Mint</button>
             </div>
         </div>
     );
---
+++ target/types/transfer_tokens.ts
@@ -0,0 +1,465 @@
+/**
+ * Program IDL in camelCase format in order to be used in JS/TS.
+ *
+ * Note that this is only a type helper and is not the actual IDL. The original
+ * IDL can be found at `target/idl/transfer_tokens.json`.
+ */
+export type TransferTokens = {
+  "address": "F1WqxTNeJo9BXmkSCeryxghRERxztXiWHVF4QTQTFpWr",
+  "metadata": {
+    "name": "transferTokens",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "createToken",
+      "discriminator": [
+        84,
+        52,
+        204,
+        228,
+        24,
+        140,
+        234,
+        75
+      ],
+      "accounts": [
+        {
+          "name": "payer",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "mintAccount",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "metadataAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  109,
+                  101,
+                  116,
+                  97,
+                  100,
+                  97,
+                  116,
+                  97
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "tokenMetadataProgram"
+              },
+              {
+                "kind": "account",
+                "path": "mintAccount"
+              }
+            ],
+            "program": {
+              "kind": "account",
+              "path": "tokenMetadataProgram"
+            }
+          }
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "tokenMetadataProgram",
+          "address": "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        },
+        {
+          "name": "rent",
+          "address": "SysvarRent111111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "tokenTitle",
+          "type": "string"
+        },
+        {
+          "name": "tokenSymbol",
+          "type": "string"
+        },
+        {
+          "name": "tokenUri",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "mintToken",
+      "discriminator": [
+        172,
+        137,
+        183,
+        14,
+        207,
+        110,
+        234,
+        56
+      ],
+      "accounts": [
+        {
+          "name": "mintAuthority",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "recipient"
+        },
+        {
+          "name": "mintAccount",
+          "writable": true
+        },
+        {
+          "name": "associatedTokenAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "recipient"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mintAccount"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "amount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "transferTokens",
+      "discriminator": [
+        54,
+        180,
+        238,
+        175,
+        74,
+        85,
+        126,
+        188
+      ],
+      "accounts": [
+        {
+          "name": "sender",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "recipient"
+        },
+        {
+          "name": "mintAccount",
+          "writable": true
+        },
+        {
+          "name": "senderTokenAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "sender"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mintAccount"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "recipientTokenAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "account",
+                "path": "recipient"
+              },
+              {
+                "kind": "const",
+                "value": [
+                  6,
+                  221,
+                  246,
+                  225,
+                  215,
+                  101,
+                  161,
+                  147,
+                  217,
+                  203,
+                  225,
+                  70,
+                  206,
+                  235,
+                  121,
+                  172,
+                  28,
+                  180,
+                  133,
+                  237,
+                  95,
+                  91,
+                  55,
+                  145,
+                  58,
+                  140,
+                  245,
+                  133,
+                  126,
+                  255,
+                  0,
+                  169
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "mintAccount"
+              }
+            ],
+            "program": {
+              "kind": "const",
+              "value": [
+                140,
+                151,
+                37,
+                143,
+                78,
+                36,
+                137,
+                241,
+                187,
+                61,
+                16,
+                41,
+                20,
+                142,
+                13,
+                131,
+                11,
+                90,
+                19,
+                153,
+                218,
+                255,
+                16,
+                132,
+                4,
+                142,
+                123,
+                216,
+                219,
+                233,
+                248,
+                89
+              ]
+            }
+          }
+        },
+        {
+          "name": "tokenProgram",
+          "address": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+        },
+        {
+          "name": "associatedTokenProgram",
+          "address": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "amount",
+          "type": "u64"
+        }
+      ]
+    }
+  ]
+};
